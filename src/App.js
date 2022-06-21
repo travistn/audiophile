43a6e8ec361fdabef8387b86a9637d9b2d5cae94
travistn <travisn714@gmail.com>
2022-06-21 16:22:12 -0700

    Navigating to a new page will automatically scroll to the top

--- src/App.js
+++ src/App.js
@@ -6,17 +6,20 @@ import Headphones from './containers/Headphones/Headphones';
 import Speakers from './containers/Speakers/Speakers';
 import Earphones from './containers/Earphones/Earphones';
 import Product from './containers/Product/Product';
+import ScrollToTop from './utils/ScrollToTop';
 
 const App = () => {
   return (
     <>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/headphones' element={<Headphones />} />
-        <Route path='/speakers' element={<Speakers />} />
-        <Route path='/earphones' element={<Earphones />} />
-        <Route path={`/product/:slug`} element={<Product />} />
-      </Routes>
+      <ScrollToTop>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/headphones' element={<Headphones />} />
+          <Route path='/speakers' element={<Speakers />} />
+          <Route path='/earphones' element={<Earphones />} />
+          <Route path={`/product/:slug`} element={<Product />} />
+        </Routes>
+      </ScrollToTop>
     </>
   );
 };
