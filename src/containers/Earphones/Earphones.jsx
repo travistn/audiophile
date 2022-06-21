b8248a1c45ac2b5d872b35e91cf10f58c582a99b
travistn <travisn714@gmail.com>
2022-06-21 15:44:38 -0700

    Users can click to the correct product page from the earphones page

--- src/containers/Earphones/Earphones.jsx
+++ src/containers/Earphones/Earphones.jsx
@@ -15,6 +15,7 @@ const Earphones = () => {
         productName='YX1 Wireless Earphones'
         description='Tailor your listening experience with bespoke dynamic drivers from the new YX1 Wireless Earphones. 
         Enjoy incredible high-fidelity sound even in noisy environments with its active noise cancellation feature.'
+        slug='yx1-earphones'
       />
       <ShopCategory />
       <MissionStatement />
