94b51538300e51ee3eba2f94af6af2e65bcf1185
travistn <travisn714@gmail.com>
2022-06-21 15:43:52 -0700

    Users can click to the correct product page from the speakers page

--- src/containers/Speakers/Speakers.jsx
+++ src/containers/Speakers/Speakers.jsx
@@ -18,6 +18,7 @@ const Speakers = () => {
         productName='ZX9 Speaker'
         description='Upgrade your sound system with the all new ZX9 active speaker. It’s a bookshelf speaker system that 
         offers truly wireless connectivity -- creating new possibilities for more pleasing and practical audio setups.'
+        slug='zx9-speaker'
       />
       <CategoryProductRight
         category='speakers'
@@ -25,6 +26,7 @@ const Speakers = () => {
         productName='ZX7 Speaker'
         description='Stream high quality sound wirelessly with minimal loss. The ZX7 bookshelf speaker uses high-end audiophile 
         components that represents the top of the line powered speakers for home or studio use.'
+        slug='zx7-speaker'
       />
       <ShopCategory />
       <MissionStatement />
